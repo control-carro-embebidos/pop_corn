bea147f3dfa3121dbfee3556191fa6a65fef3fcb
Gerardo Munoz <36160037+GerardoMunoz@users.noreply.github.com>
2025-01-03 20:41:46 -0500

    ctx

--- billar_tkinter.py
+++ billar_tkinter.py
@@ -6,16 +6,13 @@
 if __name__ == "__main__":
 
     from pop_corn.matrix import Matrix, Vec
-    from pop_corn.scene import Scene
     from pop_corn.mainscene import MainScene
     from pop_corn.disc import Disc
-    from pop_corn.py.canvas_tkinter import Canvas_Tkinter
-    import tkinter as tk
-
+    from pop_corn.py.context_tkinter import Context_Tkinter
+    
     t_anim = 50
 
-
-    master = tk.Tk()
+    ctx=Context_Tkinter()
     scene = MainScene()#bg='img/UD_@8.xbm', foreground="white", background="black")
     disc0 = Disc(scene)
     
@@ -25,14 +22,14 @@ if __name__ == "__main__":
     disc1.set_pos(-50,0)
     disc1.set_vel(Vec(5,0.5))
 
-    canvas = Canvas_Tkinter(scene, tk_master=master)
+    canvas = ctx.canvas(scene)
     scene.subscribe_canvas(canvas)
 
     def anim_loop():    
-        master.after(t_anim,anim_loop)
+        ctx.after_ms(t_anim,anim_loop)
         scene.refresh()
 
   
     anim_loop() 
-    master.mainloop()
+    ctx.mainloop()
     
